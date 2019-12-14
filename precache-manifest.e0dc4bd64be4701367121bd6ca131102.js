1fcf59be50bdb11a07ce64105ced2f48f4f5fe87
Max Girkens <maxgirkens@gmail.com>
2019-12-14 19:35:23 +0100

    Updates

---
+++ precache-manifest.e0dc4bd64be4701367121bd6ca131102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7d3c56954550e3ec9bd0d149b18976",
+    "url": "/mpc-chordfinder/index.html"
+  },
+  {
+    "revision": "f50830c730cb9fe4030f",
+    "url": "/mpc-chordfinder/static/css/main.7f65f0a5.chunk.css"
+  },
+  {
+    "revision": "bc3acfdf587534c329b5",
+    "url": "/mpc-chordfinder/static/js/2.7c3dcfa8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mpc-chordfinder/static/js/2.7c3dcfa8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f50830c730cb9fe4030f",
+    "url": "/mpc-chordfinder/static/js/main.e239d40c.chunk.js"
+  },
+  {
+    "revision": "d17149520db22fe1c730",
+    "url": "/mpc-chordfinder/static/js/runtime-main.d26248c4.js"
+  }
+]);

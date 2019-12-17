8148192585c6d398cdb990c45fa6726793121c28
Max Girkens <maxgirkens@gmail.com>
2019-12-17 20:46:09 +0100

    Updates

---
+++ precache-manifest.09710e4e21bbc7b54f0d25b0fe196e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e8633786d3d6e047f357d933312cbc",
+    "url": "/mpc-chordfinder/index.html"
+  },
+  {
+    "revision": "9137f5ee3cd43a2081b8",
+    "url": "/mpc-chordfinder/static/css/main.cf063867.chunk.css"
+  },
+  {
+    "revision": "60917dddfd8b95ef6579",
+    "url": "/mpc-chordfinder/static/js/2.67407af0.chunk.js"
+  },
+  {
+    "revision": "0ab6b95049fa63b0785dde964d67879b",
+    "url": "/mpc-chordfinder/static/js/2.67407af0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9137f5ee3cd43a2081b8",
+    "url": "/mpc-chordfinder/static/js/main.2c827e02.chunk.js"
+  },
+  {
+    "revision": "d17149520db22fe1c730",
+    "url": "/mpc-chordfinder/static/js/runtime-main.d26248c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mpc-chordfinder/precache-manifest.9a0dc6ffd9a56c0c5115efa57ddbdcc5.js"
+  "/mpc-chordfinder/precache-manifest.09710e4e21bbc7b54f0d25b0fe196e59.js"
 );
 
 self.addEventListener('message', (event) => {

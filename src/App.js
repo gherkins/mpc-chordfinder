98fe77f2c2a80eeac3e787302bb0ebb24289eccd
Max Girkens <maxgirkens@gmail.com>
2020-03-31 10:09:49 +0200

    Revert "default redirect"
    This reverts commit e5f29762704192e7ac07f61c3d63726c49f76192.

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends React.Component {
       <HashRouter basename='/'>
         <Switch>
           <Route path={'/collection/:pads/:chords'} exact component={Collection} />
-          <Route path={'/'}>
+          <Route path={'/'} exact>
             <div className="App">
               <div className="row">
                 <div className="col col-12">
